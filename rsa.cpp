b6301b0656e3c68086c16050941df7df3214b508
Himanshu Mishra <mishrah225@gmail.com>
2018-10-23 13:34:15 +0530

    Removing unnecessary calculations
    e and d were being calculated for every character of the string. 
    Now e and d will be calculated only once. One more thing, this code will work correctly only if n > 127 for character string containing alphabets.

--- rsa.cpp
+++ rsa.cpp
@@ -4,123 +4,84 @@ using namespace std;
 long long cuspow(long long a, long long b, long long n)
 {
 	long long i, temp;
-	temp =a;
-	for(i=2;i<=b;i++)
+	temp = a;
+	for(i = 2; i <= b; i++)
 	{
-		a=(a*temp) % n;
+		a = (a * temp) % n;
 	}
-
 	return a;
 }
 
 bool isPrime(long long n)
 {
 	long long i;
-	for(i=2;i*i<=n;i++)
+	for(i=2; i * i <= n; i++)
 	{
-		if(n%i==0)
-			{
-				return 0;
-			}	
+		if(n % i==0)
+        {
+            return 0;
+        }
 	}
-
 	return 1;
 }
 
-long long returnGCD( long long a , long long b)
-	{
-		long long t;
-		if(b>a)
-		{
-			t=a;
-			a=b;
-			b=t;
-		}
-	
-		while(a%b!=0)
-		{
-			t = a%b;
-			a = b;
-			b = t;
-		}
-
-		return b;
-	}
-
-
-pair<long long , char> rsa(long long plaintext , long long p , long long q)
+pair<long long , char> rsa(long long plaintext , long long e , long long d, long long n)
 {
-	long long n , phi_of_n , i , e , d  , ciphertext;
+	long long ciphertext;
 	char dplaintext;
+	ciphertext = cuspow(plaintext, e, n);
+	dplaintext = cuspow(ciphertext, d, n);
+	pair<long long , char> rpairs = make_pair(ciphertext , dplaintext);
+	return rpairs;
+}
 
-	n = p*q;
-	phi_of_n = (p-1) * (q-1);
-
-	for(i=2;i<phi_of_n/2;i++)
+int main()
+{
+	long long p , q , i, n, e, d, phi_of_n;
+	string plaintext;
+	vector<char> ciphertext;
+	do
 	{
-		if(returnGCD(i,phi_of_n)==1)
-			{
-				e = i;
-				break;
-			}
+		cout << "\nEnter first prime number: ";
+		cin >> p;
+	}while(!isPrime(p));
+	do
+	{
+		cout << "\nEnter second prime number: ";
+		cin >> q;
+	}while(!isPrime(q));
+
+	n = p * q;
+	phi_of_n = (p - 1) * (q - 1);
+	for(i = 2; i < phi_of_n / 2; i++)
+	{
+		if(__gcd(i, phi_of_n) == 1)
+		{
+			e = i;
+			break;
+		}
 	}
-
-	for(i=1 ; i<=phi_of_n;i++)
+	for(i = 1; i <= phi_of_n; i++)
 	{
-		if(((i * e) % phi_of_n) ==1)
+		if(((i * e) % phi_of_n) == 1)
 		{
 			d = i;
 			break;
 		}
 	}
-
-	ciphertext = cuspow(plaintext, e, n);	
-	dplaintext = cuspow(ciphertext, d, n);
-	pair<long long , char> rpairs = make_pair(ciphertext , dplaintext);
-
-	return rpairs;	
-}
-
-main()
-{
-	long long p , q , i;
-	string plaintext;
-	vector<char> ciphertext;
-	do
-	{
-		cout<<"\n Enter first prime number";
-		cin>>p;
-	}while(!isPrime(p));
-
-	do
-	{
-		cout<<"\n Enter second prime number";
-		cin>>q;
-	}while(!isPrime(q));
-
-	cout<<"\n Enter Plaintext ";
+	cout<<"\nEnter Plaintext ";
 	cin>>plaintext;
-
-	cout<<"\n Encrypted Text ";
-	for(i=0;i<plaintext.size();i++)
+	cout << "\nEncrypted Text ";
+	for(i = 0; i < plaintext.size(); i++)
 	{
-		pair <long long , char> rpairs = rsa(plaintext[i] , p , q);
-		cout<<rpairs.first;
+		pair <long long , char> rpairs = rsa(plaintext[i], e, d, n);
+		cout << rpairs.first;
 		ciphertext.push_back(rpairs.second);
 	}
-
-	cout<<"\n Decrypted Text ";
-
-	for(i=0;i<ciphertext.size();i++)
+	cout << "\nDecrypted Text: ";
+	for(i = 0; i < ciphertext.size(); i++)
 	{
-		cout<<ciphertext[i];
+		cout << (char)ciphertext[i];
 	}
-
-	cout<<endl;
+	cout << endl;
 }
-	
-
-	
-		
-
-	
